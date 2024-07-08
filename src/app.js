2ddc0cf2fe870e66db3b6d1abd0f0f7aa315f7fe
PinchaoRamiro <antonio.pinchao@utp.edu.co>
2024-07-08 03:35:14 -0500

    Algunos errores

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 
 import cookieParser from 'cookie-parser';
@@ -13,6 +14,11 @@ import cardRoutes from './routes/cardRoutes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const corsOptions = {
+  origin: '*', // Reemplaza con la URL de tu dominio o dominios permitidos
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
 
 
 dotenv.config();
@@ -30,9 +36,25 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use('/api', userRoutes);// ruta para los usuarios
 app.use('/api', columnRoutes);// ruta para las columnas
 app.use('/api', cardRoutes);// ruta para las tarjetas
+
+//manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
+//errores del cliente por fallas en syntax o en la BD
+app.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    res.status(400).json({ error: err.message });
+  } else {
+    next(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
